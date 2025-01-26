92006260fb07c5508b122ddb46cb2fd8887471db
asadpisatel <berdibaev.asadbek@gmail.com>
2025-01-26 12:03:47 +0500

    some change

--- src/axiosInstance.js
+++ src/axiosInstance.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const instance = axios.create({
   baseURL:
     process.env.NODE_ENV === "production"
-      ? "https://https://theappserver.onrender.com"
+      ? "https://theappserver.onrender.com"
       : "http://localhost:5000",
 });
 
